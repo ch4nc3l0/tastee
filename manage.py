b1bf2ac4213884a48b2ed4f2ba18b874ebdba37a
chance <8172146+ch4nc3l0@users.noreply.github.com>
2019-02-08 01:18:13 -0500

    Fix: db migrater

--- manage.py
+++ manage.py
@@ -12,9 +12,21 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+class Restaurant(db.Model):
+    __tablename__ = 'restaurant'
+    name = db.Column(db.String(80), nullable = False)
+    id = db.Column(db.Integer, primary_key = True)
+
+
+class MenuItem(db.Model):
+    __tablename__ = 'menuItem'
+    course = db.Column(db.String(250))
+    description = db.Column(db.String(250))
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+    price = db.Column(db.String(8))
+    restaurant = db.relationship(Restaurant)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
 
 if __name__ == '__main__':
     manager.run()
