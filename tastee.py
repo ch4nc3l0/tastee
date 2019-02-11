8d8e91607e374b119ec09e9c3c71af04fbeeb6ed
chance <8172146+ch4nc3l0@users.noreply.github.com>
2019-02-11 16:15:08 -0500

    Feat: add basic login

--- tastee.py
+++ tastee.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3.7.1
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask import Flask, request, url_for, render_template, redirect, session
-import random, string, os
+from flask import Flask, request, url_for, render_template, redirect, session, make_response
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+import random, string, os, json, httplib2, requests
+
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 
 
 app = Flask(__name__)
@@ -130,6 +134,74 @@ def deleteMenuItem(restaurant_id, menuItem_id):
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
-    session["state"] = state
+    login_session['state'] = state
     return render_template("login.html", STATE=state)
-    
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid State'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentails = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response= make_response(json.dumps('Failed to upgrad auth code'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    access_token = credentails.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h=httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+
+    gplus_id = credentails.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps('Tokens user id does not match given user id'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response   
+
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps('Tokens client id does not match apps'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response   
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print ("done!")
+    return output
